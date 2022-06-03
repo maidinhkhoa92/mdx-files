565c9256ea22fad96472a6d9c2996d49501e7bf1
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2022-06-03 22:05:04 +0700

    Fix file

---
+++ components/editor.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react"
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+const Editor: FC = () => {
+    return (
+      <SimpleMDE />
+    )
+  }
+
+export default Editor
